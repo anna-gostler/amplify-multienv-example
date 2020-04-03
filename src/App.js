129510f9533577e2ce208d014da029768b1d2aa8
anna-gostler <anna-gostler@users.noreply.github.com>
2020-04-03 23:12:52 +0200

    change

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ Amplify.configure(awsConfig);
 
 const ListView = ({ todos }) => (
   <div>
-    <h2>All Todos</h2>
+    <h2>List of Todos</h2>
     <ul>
       {todos.map(todo => (
         <li key={todo.id}>
